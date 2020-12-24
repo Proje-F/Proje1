dbe09c33cd5cf94f59bdadecacb1d1d83e85e023
Bilgenur Kara <47607238+bilgekara@users.noreply.github.com>
2020-12-24 05:09:17 +0300

    updated

--- constants/colors.js
+++ constants/colors.js
@@ -1,7 +1,7 @@
 const background = '#171D32';
 const statusBar = '#FFF';
 const primary = '#171D32';
-const topNavText = '#FFF';
+const topNavText = '#664B7C';
 const inActiveTab = 'rgb(143, 155, 179)';
 const ActiveTab = '#664B7C';
 export default {
